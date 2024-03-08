fb7bc1c9c48a3667f278026e60f074e34b249c60
Abhishek Rajpal <86553778+Abhishek2110@users.noreply.github.com>
2024-03-08 14:50:06 +0530

    [Abhishek] Added . Hoisting

--- practice.js
+++ practice.js
@@ -18,19 +18,19 @@ console.log(divide(21, 7))
 
 // Variables in JS
 
-// var
+// var (global scoped)
 var a = 21;
 var a = "Abhishek";
 a = 24;
 console.log(a)
 
-// let
+// let (block scoped)
 let b = 22;
 // let b = 23; // We can't do this
 b = 23;
 console.log(b)
 
-// const
+// const  (block scoped)
 const c = 25;
 // c = 26; // We can't do this
 console.log(c);
@@ -38,17 +38,34 @@ console.log(c);
 // Scoping
 
 // We can redeclare or initialize same variable name in different block scopes
-{
-  var a = 24;
-  console.log(a);
+// global scope
+var h = "hello";
+let g = "Welcome";
+const j = "Bye";
+// block scope
+if (true) {
+  var h = "hi";
+  let g = "How are you?";
+  const j = "See you";
+  console.log(h)
+  console.log(g)
+  console.log(j)
 }
+console.log(h)
+console.log(g)
+console.log(j)
 
-{
-  let b = 23;
-  console.log(b);
-}
+// Hoisting
 
-{
-  const c = 25;
-  console.log(c);
-}
+// Hoisting using var keyword
+console.log(counter);
+var counter = 1;
+
+// not possible in let
+// console.log(count);
+// let count = 1;
+
+// not possible in const
+// console.log(age);
+// const age = 1;
+
