33e9ce61ec9f13a039430d08cb9539b73fb9f52f
Abhishek Rajpal <86553778+Abhishek2110@users.noreply.github.com>
2024-03-08 12:13:44 +0530

    [Abhishek] Added . Functions in JS

---
+++ practice.js
@@ -0,0 +1,17 @@
+// Functions in JS
+
+// Normal Function
+function sum(a, b){
+    return a + b;
+}
+console.log(sum(15, 21))
+
+// Arrow Function
+const multiply = (c, d) => c * d;
+console.log(multiply(5, 7))
+
+// Anonymous Function
+const divide = function (x, y) {
+    return x / y;
+  };
+console.log(divide(21, 7))
