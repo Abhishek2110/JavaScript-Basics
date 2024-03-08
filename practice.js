f892df31d91fc4dbbb9b3dda0865291743dc00b1
Abhishek Rajpal <86553778+Abhishek2110@users.noreply.github.com>
2024-03-08 15:39:21 +0530

    [Abhishek] Added . Callbacks

--- practice.js
+++ practice.js
@@ -94,3 +94,21 @@ console.log("Before non-blocking operation");
 nonBlockingOperation();
 console.log("After non-blocking operation");
 
+// Callbacks
+
+function isOdd(number) {
+  return number % 2 != 0;
+}
+
+function filter(numbers, fn) {
+  let results = [];
+  for (const number of numbers) {
+    if (fn(number)) {
+      results.push(number);
+    }
+  }
+  return results;
+}
+let numbers = [1, 2, 4, 7, 3, 5, 6];
+
+console.log(filter(numbers, isOdd));
